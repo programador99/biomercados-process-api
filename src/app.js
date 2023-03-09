3f87866162fae9a473f32a5dee51b1d12ba8ae3c
d34ckgler <herogamers.ortiz@gmail.com>
2023-03-09 09:09:03 -0400

    Adjust port listen

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.set('port', process.env.PORT || 3002)
+app.set('port', process.env.PORT || 3003)
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Express' });
